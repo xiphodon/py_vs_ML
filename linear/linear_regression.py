7268c86bcca63e9a9aa633d5a03928fd2f9947bf
Xiphodon <827789895@qq.com>
2017-04-11 10:49:48 +0800

    岭回归

--- linear/linear_regression.py
+++ linear/linear_regression.py
@@ -7,6 +7,10 @@
 # @Software: PyCharm Community Edition
 
 
+'''
+线性回归
+'''
+
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model, model_selection
@@ -19,7 +23,7 @@ def load_data():
     '''
     diabetes = datasets.load_diabetes()  # 使用 scikit-learn 自带的一个糖尿病病人的数据集
     return model_selection.train_test_split(diabetes.data, diabetes.target, test_size=0.25,
-                                             random_state=0)  # 拆分成训练集和测试集，测试集大小为原始数据集大小的 1/4
+                                            random_state=0)  # 拆分成训练集和测试集，测试集大小为原始数据集大小的 1/4
 
 
 def test_LinearRegression(*data):
@@ -29,9 +33,9 @@ def test_LinearRegression(*data):
     :return: None
     '''
     X_train, X_test, y_train, y_test = data
-    regr = linear_model.LinearRegression(fit_intercept=True,normalize=True,n_jobs=-1)   # 参数：计算截距b，正则化，指定cpu数量（-1所有可用）
+    regr = linear_model.LinearRegression(fit_intercept=True, normalize=True, n_jobs=-1)  # 参数：计算截距b，正则化，指定cpu数量（-1所有可用）
     regr.fit(X_train, y_train)
-    print('Coefficients:%s, intercept %.2f' % (regr.coef_, regr.intercept_))    #regr.coef_ 权重向量 ， regr.intercept_ 截距b
+    print('Coefficients:%s, intercept %.2f' % (regr.coef_, regr.intercept_))  # regr.coef_ 权重向量 ， regr.intercept_ 截距b
     print("Residual sum of squares: %.2f" % np.mean((regr.predict(X_test) - y_test) ** 2))  # 均方误差
     print('Score: %.2f' % regr.score(X_test, y_test))
 
