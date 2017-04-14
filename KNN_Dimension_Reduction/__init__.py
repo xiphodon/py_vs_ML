07b73f52623e12a811bc31634f2103bc80563ccd
Xiphodon <827789895@qq.com>
2017-04-14 14:36:43 +0800

    kNN分类，回归

---
+++ KNN_Dimension_Reduction/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/4/14 10:50
+# @Author  : GuoChang
+# @Site    : https://github.com/xiphodon
+# @File    : __init__.py.py
+# @Software: PyCharm Community Edition
